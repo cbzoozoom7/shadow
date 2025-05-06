1d1da2a6ea7b47ea3fca6dd7ef5a9d1943534713
Chris <55931405+cbzoozoom7@users.noreply.github.com>
2025-05-06 08:31:41 -0500

    Built a testing rig that imports just one eclipse

--- Shadow/ContentView.swift
+++ Shadow/ContentView.swift
@@ -13,11 +13,22 @@ enum loadingState {
 }
 struct ContentView: View {
     @State var selectedEclipse: Eclipse? = nil
-    @State var eclipsePickerShown: Bool = true
     @StateObject var eclipses = EclipseModel()
     var body: some View {
-        Map()
-            .mapStyle(.hybrid)
+        ZStack {
+            Map()
+                .mapStyle(.hybrid)
+                .disabled(eclipses.loading)
+            Color.gray
+                .ignoresSafeArea()
+                .opacity(eclipses.loading ? 0.8 : 0)
+                .disabled(!eclipses.loading)
+            ProgressView()
+                .disabled(!eclipses.loading)
+                .opacity(eclipses.loading ? 1 : 0)
+            Text(String(describing: eclipses.eclipses))
+                .background()
+        }
     }
 }
 
--- Shadow/Eclipse.swift
+++ Shadow/Eclipse.swift
@@ -8,6 +8,7 @@
 import Foundation
 import MapKit
 import SwiftData
+import OSLog
 
 enum HybridType {
     case startEndAnnular
@@ -53,31 +54,67 @@ struct Eclipse: Identifiable {
     let tanUmbralAxisAngle: Double // tan(f2)
 }
 class EclipseModel: ObservableObject {
-    @Published var eclipses: [Int:Eclipse] = [:]
+    @Published var eclipses: [Int:Eclipse]
+    @Published var loading: Bool
+    init() {
+        eclipses = [:]
+        loading = true
+        let _ = Task {
+            do {
+                try await self.load()
+            } catch {
+                
+            }
+            loading = false
+        }
+    }
     func load() async throws {
-        guard let filepath = Bundle.main.path(forResource: "filtered-eclipses", ofType: "csv") else {
-            throw NSError(domain: "", code: 0, userInfo: nil)
+        let logger = Logger()
+        guard let filepath = Bundle.main.path(forResource: "test-eclipse", ofType: "csv") else {
+            return
         }
         let content = try String(contentsOfFile: filepath, encoding: .ascii)
         var lines = content.components(separatedBy: "\n")
+        logger.notice("lines: \(lines.count)")
+        assert(lines.count > 0)
+        
         lines.removeFirst() // Discard header row
         for line in lines {
-            var tokens = line.components(separatedBy: ",")
+            guard !line.isEmpty else { continue }
+            let tokens = line.components(separatedBy: ",")
+            logger.notice("tokens.count: \(tokens.count)")
             for var token in tokens {
                 if let quotedSection = token.firstMatch(of: /"(.*)"/) {
                     token = String(quotedSection.1)
                 }
             }
+            logger.notice("tokens.count: \(tokens.count)")
+            assert(tokens.count > 40)
+            
             var time = {
                 let dateFormatter = DateFormatter()
                 dateFormatter.dateFormat = "yyyy MM dd HH:mm:ss z"
-                return dateFormatter.date(from: "\(tokens[0]) \(tokens[1]) \(tokens[2]) \(tokens[3]) UTC")!
+                let dateString = "\(tokens[0]) \(tokens[1]) \(tokens[2]) \(tokens[3]) UTC"
+                logger.notice("dateString: \(dateString)")
+                let date = dateFormatter.date(from: "\(tokens[0]) \(tokens[1]) \(tokens[2]) \(tokens[3]) UTC") ?? Date()
+                logger.notice("time: \(date)")
+                return date
             }()
+            
             if let deltaT = Double(tokens[4]) {
+                logger.notice("tokens[4]: \(tokens[4])\t-> deltaT: \(deltaT)")
                 time.addTimeInterval(deltaT)
+                logger.notice("time: \(time)")
+            } else {
+                logger.notice("tokens[4]: \(tokens[4])\tfailed to parse as Double")
             }
-            let luna = Int(tokens[5])!
+            
+            let luna = Int(tokens[5]) ?? 0
+            logger.notice("tokens[5]: \(tokens[5])\t-> luna: \(luna)")
+            
             let saros = Int(tokens[6])!
+            logger.notice("tokens[6]: \(tokens[6])\t-> saros: \(saros)")
+            
             let type: EclipseType = {
                 switch tokens[7].first {
                 case "P":
@@ -89,6 +126,7 @@ class EclipseModel: ObservableObject {
                 case "H":
                     let hybridType: HybridType
                     let index = tokens[7].index(tokens[7].startIndex, offsetBy: 1)
+                    
                     switch tokens[7][index] {
                     case "2":
                         hybridType = .startTotal
@@ -99,44 +137,98 @@ class EclipseModel: ObservableObject {
                     }
                     return .hybrid(hybridType)
                 default:
-                    fatalError()
+                    logger.error("Failed to parse eclipse type")
+                    return .partial
                 }
             }()
-            let gamma = Double(tokens[8])!
-            let magnitude = Double(tokens[9])!
+            logger.notice("tokens[7]: \(tokens[7])\t-> type: \(String(describing: (type)))")
+            
+            let gamma = Double(tokens[8]) ?? 0
+            logger.notice("tokens[8]: \(tokens[8])\t-> gamma: \(gamma)")
+            
+            let magnitude = Double(tokens[9]) ?? 0
+            logger.notice("tokens[9]: \(tokens[9])\t-> magnitude: \(magnitude)")
             // tokens[10] is latitude
             // tokens[11] is longitude
             let latitude = Double(tokens[12])! // tokens[12] is in decimal format unlike 10
+            logger.notice("tokens[12]: \(tokens[12])\t-> latitude: \(latitude)")
+            
             let longitude = Double(tokens[13])!
+            logger.notice("tokens[13]: \(tokens[13])\t-> longitude: \(longitude)")
+            
             let sunAltitude: Double?
             if case .partial = type {
                 sunAltitude = nil
             } else {
-                sunAltitude = Double(tokens[14])!
+                sunAltitude = Double(tokens[14]) ?? 0
             }
-            let sunAzimuth = Double(tokens[15])!
+            logger.notice("tokens[14]: \(tokens[14])\t-> sunAltitude: \(String(describing: sunAltitude))")
+            
+            let sunAzimuth = Double(tokens[15]) ?? 0
+            logger.notice("tokens[15]: \(tokens[15])\t-> sunAzimuth: \(sunAzimuth)")
+            
             let pathWidth: Double?
             let duration: Double?
             if case .partial = type {
                 pathWidth = nil
                 duration = nil
             } else {
-                pathWidth = Double(tokens[16])!
+                pathWidth = Double(tokens[16]) ?? 0
                 // tokens[17] is duration
-                duration = Double(tokens[18])! // tokens[18] is in decimal format
+                duration = Double(tokens[18]) ?? 0 // tokens[18] is in decimal format
             }
-            let catalogNumber = Int(tokens[19])!
+            logger.notice("tokens[16]: \(tokens[16])\t-> pathWidth: \(String(describing: pathWidth))")
+            logger.notice("tokens[18]: \(tokens[18])\t-> duration: \(String(describing: duration))")
+            
+            let catalogNumber = Int(tokens[19]) ?? 0
+            logger.notice("tokens[19]: \(tokens[19])\t-> catalogNumber: \(catalogNumber)")
             // tokens[20] is canon plate number
             // tokens[21] is Julian date
-            let t0 = Double(tokens[22])!
-            let x = [Double(tokens[23])!, Double(tokens[24])!, Double(tokens[25])!, Double(tokens[26])!]
-            let y = [Double(tokens[27])!, Double(tokens[28])!, Double(tokens[29])!, Double(tokens[30])!]
-            let declination = [Double(tokens[31])!, Double(tokens[32])!, Double(tokens[33])!]
-            let mu = [Double(tokens[34])!, Double(tokens[35])!, Double(tokens[36])!]
-            let l1 = [Double(tokens[37])!, Double(tokens[38])!, Double(tokens[39])!]
-            let l2 = [Double(tokens[40])!, Double(tokens[41])!, Double(tokens[42])!]
-            let tanF1 = Double(tokens[43])!
-            let tanF2 = Double(tokens[44])!
+            let t0 = Double(tokens[22]) ?? 0
+            logger.notice("tokens[22]: \(tokens[22])\t-> t0: \(t0)")
+            
+            var x: [Double] = []
+            for token in tokens[23...26] {
+                x.append(Double(token) ?? 0)
+            }
+            logger.notice("tokens[23..26]: \(tokens[23...26])\t-> x: \(x)")
+            
+            var y: [Double] = []
+            for token in tokens[27...30] {
+                y.append(Double(token) ?? 0)
+            }
+            logger.notice("tokens[27..30]: \(tokens[27...30])\t-> y: \(y)")
+            
+            var declination: [Double] = []
+            for token in tokens[31...33] {
+                declination.append(Double(token) ?? 0)
+            }
+            logger.notice("tokens[31..33]: \(tokens[31...33])\t-> declination: \(declination)")
+            
+            var mu: [Double] = []
+            for token in tokens[34...36] {
+                mu.append(Double(token) ?? 0)
+            }
+            logger.notice("tokens[34..36]: \(tokens[34...36])\t-> mu: \(mu)")
+            
+            var l1: [Double] = []
+            for token in tokens[37...39] {
+                l1.append(Double(token) ?? 0)
+            }
+            logger.notice("tokens[37..39]: \(tokens[37...39])\t-> l1: \(l1)")
+            
+            var l2: [Double] = []
+            for token in tokens[40...42] {
+                l2.append(Double(token) ?? 0)
+            }
+            logger.notice("tokens[40..42]: \(tokens[40...42])\t-> l2: \(l2)")
+            
+            let tanF1 = Double(tokens[43]) ?? 0
+            logger.notice("tokens[43]: \(tokens[43])\t-> tanF1: \(tanF1)")
+            
+            let tanF2 = Double(tokens[44]) ?? 0
+            logger.notice("tokens[44]: \(tokens[44])\t-> tanF2: \(tanF2)")
+            
             let newEclipse = Eclipse(
                 time: time,
                 luna: luna,
